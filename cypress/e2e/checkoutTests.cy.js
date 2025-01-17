fff0957093e22591bed7d0b8269efb26b186751d
dashboy <dashiellboyles@hotmail.com>
2025-01-17 11:48:25 +0000

    unsuccessful tests added

--- cypress/e2e/checkoutTests.cy.js
+++ cypress/e2e/checkoutTests.cy.js
@@ -12,7 +12,7 @@ describe('The checkout process tests', () => {
     cy.get('[data-test="checkout"]').click()
     cy.url().should('include', '/checkout-step-one.html');
     cy.get('#first-name').type('Dash')
-    cy.get('#last-name').type('Dash')
+    cy.get('#last-name').type('Boyles')
     cy.get('#postal-code').type('BR7 6JP')
     cy.get('[data-test="continue"]').click()
 
@@ -24,5 +24,40 @@ describe('The checkout process tests', () => {
     cy.get('[data-test="finish"]').click()
     cy.get('[data-test="checkout-complete-container"]').should('contain', 'Thank you for your order!')
     cy.get('[data-test="shopping-cart-link"]').should('be.empty');
+  })
+
+  it('should not allow you to complete checkout if postcode is left blank', () => {
+    cy.get('[data-test="checkout"]').click()
+    cy.get('#first-name').type('Dash')
+    cy.get('#last-name').type('Boyles')
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="checkout-info-container"]').should('contain', 'Error: Postal Code is required')
+  })
+
+  it('should not allow you to complete checkout if first name is left blank', () => {
+    cy.get('[data-test="checkout"]').click()
+    cy.get('#last-name').type('Boyles')
+    cy.get('#postal-code').type('BR7 6JP')
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="checkout-info-container"]').should('contain', 'Error: First Name is required')
+  })
+
+  it('should not allow you to complete checkout if last name is left blank', () => {
+    cy.get('[data-test="checkout"]').click()
+    cy.get('#first-name').type('Dash')
+    cy.get('#postal-code').type('BR7 6JP')
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="checkout-info-container"]').should('contain', 'Error: Last Name is required')
+  })
+
+  it('should allow you to correct an error', () => {
+    cy.get('[data-test="checkout"]').click()
+    cy.get('#first-name').type('Dash')
+    cy.get('#postal-code').type('BR7 6JP')
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="checkout-info-container"]').should('contain', 'Error: Last Name is required')
+    cy.get('#last-name').type('Boyles')
+    cy.get('[data-test="continue"]').click()
+    cy.url().should('include', '/checkout-step-two.html')
   })
 })
