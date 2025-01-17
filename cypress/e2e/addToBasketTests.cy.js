3a23a578941f6c4f556e143d12f2c3f7b9906d93
dashboy <dashiellboyles@hotmail.com>
2025-01-17 10:12:15 +0000

    added test for icon appearing with number of items

---
+++ cypress/e2e/addToBasketTests.cy.js
@@ -0,0 +1,27 @@
+describe('Add to basket Tests', () => {
+  beforeEach(() => {
+    cy.visit('https://saucedemo.com')
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click()
+  })
+
+  it('should have nothing in basket originally', () => {
+    cy.get('[data-test="shopping-cart-link"]').click()
+    cy.url().should('include', '/cart.html')
+    cy.get('.cart_item').should('not.exist');
+  })
+
+  it('the icon should update with an item in the basket', () => {
+    cy.get('.inventory_item').first().find('button').click();
+    cy.get('.shopping_cart_badge').should('contain', '1');
+
+    cy.get('.inventory_item').last().find('button').click();
+    cy.get('.shopping_cart_badge').should('contain', '2');
+
+    cy.get('.inventory_item_name').contains('Sauce Labs Fleece Jacket')
+    .parents('.inventory_item')
+    .find('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click()
+    cy.get('.shopping_cart_badge').should('contain', '3');
+  })
+})
--- cypress/e2e/swaglabs.cy.js
+++ cypress/e2e/swaglabs.cy.js
@@ -27,25 +27,10 @@ describe('Login Tests', () => {
 
   it('should show an error for when a wrong password is used', () => {
     cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('hello sunshine');
+    cy.get('#password').type('HelloSunshine');
 
     cy.get('#login-button').click()
 
     cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service')
-  })
-})
-
-describe('Add to basket Tests', () => {
-  beforeEach(() => {
-    cy.visit('https://saucedemo.com')
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button').click()
-  })
-
-  it('should have nothing in basket originally', () => {
-    cy.get('[data-test="shopping-cart-link"]').click()
-    cy.url().should('include', '/cart.html')
-    cy.get('.cart_item').should('not.exist');
   })
 })
