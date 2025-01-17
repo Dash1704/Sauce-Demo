f05a5cf93a3829ccb30e2c750d37a892a04d1d1a
dashboy <dashiellboyles@hotmail.com>
2025-01-17 09:32:39 +0000

    refactored tests with before each

--- cypress/e2e/swaglabs.cy.js
+++ cypress/e2e/swaglabs.cy.js
@@ -1,20 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Login Tests', () => {
+  beforeEach(() => {
     cy.visit('https://saucedemo.com')
+  })
+
+  it('should allow user to login with first username', () => {
     cy.get('#user-name').type('standard_user');
     cy.get('#password').type('secret_sauce');
-
 
     cy.get('#login-button').click()
 
     cy.url().should('include', '/inventory.html');
     cy.get('#react-burger-menu-btn').should('exist');
-    cy.get('.shopping_cart_link').should('exist');
-    
+    cy.get('.shopping_cart_link').should('exist');    
   })
 
   it('should be able to login with last username', () => {
-    cy.visit('https://saucedemo.com')
     cy.get('#user-name').type('visual_user');
     cy.get('#password').type('secret_sauce');
 
@@ -26,7 +26,6 @@ describe('template spec', () => {
   })
 
   it('should show an error for when a wrong password is used', () => {
-    cy.visit('https://saucedemo.com')
     cy.get('#user-name').type('standard_user');
     cy.get('#password').type('hello sunshine');
 
