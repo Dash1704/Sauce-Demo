c83421ea93cd46d24ba2176bf0ceac37a2fdb735
dashboy <dashiellboyles@hotmail.com>
2025-01-19 12:46:41 +0000

    changed to data-tests for add to basket tests

--- cypress/e2e/addToBasketTests.cy.js
+++ cypress/e2e/addToBasketTests.cy.js
@@ -9,43 +9,43 @@ describe('Add to basket Tests', () => {
   it('should have nothing in basket originally', () => {
     cy.get('[data-test="shopping-cart-link"]').click()
     cy.url().should('include', '/cart.html')
-    cy.get('.cart_item').should('not.exist');
+    cy.get('[data-test="inventory-item"]').should('not.exist');
   })
 
   it('the icon should update with an item in the basket', () => {
-    cy.get('.inventory_item').first().find('button').click();
+    cy.get('[data-test="inventory-item"]').first().find('button').click();
     cy.get('[data-test="shopping-cart-badge"]').should('contain', '1');
 
-    cy.get('.inventory_item').last().find('button').click();
+    cy.get('[data-test="inventory-item"]').last().find('button').click();
     cy.get('[data-test="shopping-cart-badge"]').should('contain', '2');
 
-    cy.get('.inventory_item').contains('Sauce Labs Fleece Jacket')
-    .parents('.inventory_item')
+    cy.get('[data-test="inventory-item"]').contains('Sauce Labs Fleece Jacket')
+    .parents('[data-test="inventory-item"]')
     .find('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click()
     cy.get('[data-test="shopping-cart-badge"]').should('contain', '3');
   })
 
   it('should show an item clicked in the checkout page', () => {
-    cy.get('.inventory_item').first().find('button').click();
+    cy.get('[data-test="inventory-item"]').first().find('button').click();
     cy.get('[data-test="shopping-cart-badge"]').click()
     cy.url().should('include', '/cart.html')
-    cy.get('.cart_item').should('exist');
+    cy.get('[data-test="inventory-item"]').should('exist');
   })
 
   it('should show multiple items in the checkout page', () => {
-    cy.get('.inventory_item').first().find('button').click();
-    cy.get('.inventory_item').last().find('button').click();
+    cy.get('[data-test="inventory-item"]').first().find('button').click();
+    cy.get('[data-test="inventory-item"]').last().find('button').click();
     cy.get('[data-test="shopping-cart-badge"]').click()
     cy.url().should('include', '/cart.html')
-    cy.get('.cart_list').find('.cart_item').should('have.length', 2);
+    cy.get('[data-test="cart-list"]').find('[data-test="inventory-item"]').should('have.length', 2);
   })
 
   it('should remove an item from the checkout page successfully', () => {
-    cy.get('.inventory_item').first().find('button').click();
-    cy.get('.inventory_item').last().find('button').click();
+    cy.get('[data-test="inventory-item"]').first().find('button').click();
+    cy.get('[data-test="inventory-item"]').last().find('button').click();
     cy.get('[data-test="shopping-cart-badge"]').click()
-    cy.get('.cart_item').first().find('button').click()
-    cy.get('.cart_list').find('.cart_item').should('have.length', 1)
+    cy.get('[data-test="inventory-item"]').first().find('button').click()
+    cy.get('[data-test="cart-list"]').find('[data-test="inventory-item"]').should('have.length', 1)
   })
 })
 
